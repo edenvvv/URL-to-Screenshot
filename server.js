93392ae575136cc1561c5407b15f12d267667573
eden <57102615+edenvvv@users.noreply.github.com>
2021-07-18 17:15:20 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const Screenshot = require('url-to-screenshot');
+const fs = require('fs');
+const open = require('open');
+var input_url = require('./input.json');
+var output = {};
+var key = 'file';
+
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  const s = new Screenshot(input_url.url)
+    .capture()
+    .then(img => {
+      fs.writeFileSync(__dirname + '/video.png', img);
+      output[key] = __dirname + '/video.png';
+      var json = JSON.stringify(output);
+      fs.writeFile('output.json', json, 'utf8', function (err) {
+        if (err) throw err;
+      });
+      console.log('complete');
+    })
+  res.end(input_url.url);
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
+open('http://localhost:8080');
